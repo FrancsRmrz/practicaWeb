674cda8804aff313ae12ba5988d1ffe1186b9638
FrancsRmrz <francisco.ramirez.en@outlook.com>
2023-11-29 08:57:48 -0600

    a

---
+++ config/config.php
@@ -0,0 +1,4 @@
+<?php
+define('CLIENT_ID', 'AUZ6750Jyb-9PygjQ8PAZGxonok_vCtaE3_yRg1cIxeqwIPpHIEnkGds5sSIA7XstlXqY2bDKeg5uvMu');
+define('LOCALE', 'es_ES');
+?>
---
+++ src/admin/includes/footer.php
@@ -0,0 +1,61 @@
+</div>
+<!-- /.container-fluid -->
+
+</div>
+<!-- End of Main Content -->
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" type="text/css" href="admin.css">
+    <link rel="stylesheet" type="text/css" href="estilos.css">
+</head>
+
+<!-- Footer -->
+    <footer class="ftco-footer">
+      <div class="container-fluid px-0 py-5 bg-black">
+      	<div class="container">
+      		<div class="row">
+	          <div class="col-md-12">
+              <div style="text-align: center;">
+                <p class="mb-0" style="color: rgba(255,255,255,.5);">
+                    Copyright &copy;<script>document.write(new Date().getFullYear());</script> | Todos los derechos reservados | HotDirt Software
+                </p>
+            </div>
+            
+	        </div>
+      	</div>
+      </div>
+    </footer>
+<!-- End of Footer -->
+
+
+</div>
+<!-- End of Content Wrapper -->
+
+</div>
+<!-- End of Page Wrapper -->
+
+<!-- Scroll to Top Button-->
+<a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+</a>
+
+
+<!-- Bootstrap core JavaScript-->
+<script src="../assets/js/jquery-3.6.0.min.js"></script>
+<script src="../assets/js/bootstrap.bundle.min.js"></script>
+
+<!-- Core plugin JavaScript-->
+<script src="../assets/js/jquery.easing.min.js"></script>
+
+<!-- Custom scripts for all pages-->
+<script src="../assets/js/sb-admin-2.min.js"></script>
+<script src="../assets/js/all.min.js"></script>
+<script src="../assets/js/scripts.js"></script>
+
+
+</body>
+
+</html>
